af347488b3bc91c4f0513791d31a9276e3433c3a
Reza.M.S.A <rezamoradi0933@gmail.com>
2025-05-26 19:47:01 +0330

    sdfsadf

--- my-app/src/Components/Movies.js
+++ my-app/src/Components/Movies.js
@@ -6,7 +6,7 @@ class Movies extends Component {
   constructor() {
     super();
     this.thisIndex = 1;
-    console.log("TEST 4");
+    console.log("TEST 5");
     this.state = {
       moviesJson: null,
       catecorys: [],
